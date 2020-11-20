505c90085a0d2610fe665dc24e758dbe139b0fc1
张帅 <31801150@stu.zucc.edu.cn>
2020-11-20 13:55:32 +0800

    Update index.js

--- demo-1/pages/index/index.js
+++ demo-1/pages/index/index.js
@@ -20,28 +20,18 @@ Page({
 
     ask:"    ",
     askcolor:"green",
-   question :[
-     {
-        text:["红色"],
-        color:["yellow"],
-        q:"之前出现的文字是什么？",
-        choose:["红色","黄色"],
-        true:0
-     },{
-        text:["灰色"],
-        color:["green"],
-        q:"之前出现的文字是什么颜色？",
-        choose:["灰色","黄色","绿色"],
-        true:2
-     },{
-      text:["蓝色","红色"],
-      color:["green","purple"],
-      q:"之前出现的第二个文字是什么？",
-      choose:["蓝色","红色","绿色","紫色"],
-      true:1
-     }
-   ],
-   indexq:2,
+  
+    alltext:["红色","蓝色","紫色","绿色","橙色","黄色"],
+    allcolor:["red","skyblue","purple","green","orange","yellow"],
+    allq:["之前出现的文字是什么？","之前出现的文字是什么颜色？","出现的第一个文字是什么？","出现的第一个文字是什么颜色？","出现的第二个文字是什么？","出现的第二个文字是什么颜色？"],
+    text:[],
+    color:[],
+    q:'',
+    choose:[],
+    true:0,
+    indexq:0,
+    ans:'',
+    right:0
 
   },
   bindViewTap: function() {
@@ -56,30 +46,312 @@ Page({
 
 
   show:function() {
-        this.setData({
-          show:true,
-          showbu:false
+
+   if(this.data.indexq<5)
+   {
+    var texttemp=[];
+    texttemp.push(Math.floor(Math.random()*6));
+    var colortemp=[];
+    colortemp.push(Math.floor(Math.random()*6));
+    var choosetemp=[];
+    var qtemp=this.data.allq[Math.floor(Math.random()*2)];
+     if(qtemp=="之前出现的文字是什么？")
+     {
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+          
+             if(choosetemp[j]==num||num==texttemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+ 
+       
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+       }
+       choosetemp[Math.floor(Math.random()*4)]=texttemp[0];
+       this.setData({
+         ans:texttemp[0],
         });
-        var that=this;
-        setTimeout(function () {
-          that.setData({
-            show:false,
-            showq:true
-          });
-        that.start();
-         }, 1000) //延迟时间 这里是1秒
+     }else{
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+        
+             if(choosetemp[j]==num||num==colortemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+ 
+        
+       }
+       choosetemp[Math.floor(Math.random()*4)]=colortemp[0];
+       this.setData({
+        ans:colortemp[0],
+       });
+     }
+ 
+ 
+    
+         this.setData({
+           show:true,
+           showbu:false,
+           text:texttemp,
+           color:colortemp,
+           choose:choosetemp,
+           q:qtemp
+         });
+        
+         var that=this;
+         setTimeout(function () {
+           that.setData({
+             show:false,
+             showq:true
+           });
+         that.start();
+          }, 3000) //延迟时间 这里是1秒
+   }else if(this.data.indexq<10){
+    var texttemp=[];
+    texttemp.push(Math.floor(Math.random()*6));
+    var colortemp=[];
+    colortemp.push(Math.floor(Math.random()*6));
+    var choosetemp=[];
+    var qtemp=this.data.allq[Math.floor(Math.random()*2)];
+     if(qtemp=="之前出现的文字是什么？")
+     {
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+          
+             if(choosetemp[j]==num||num==texttemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+ 
+       
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+       }
+       choosetemp[Math.floor(Math.random()*4)]=texttemp[0];
+       this.setData({
+         ans:texttemp[0],
+        });
+     }else{
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+         
+             if(choosetemp[j]==num||num==colortemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+ 
+        
+       }
+       choosetemp[Math.floor(Math.random()*4)]=colortemp[0];
+       this.setData({
+        ans:colortemp[0],
+       });
+     }
+ 
+ 
+    
+         this.setData({
+           show:true,
+           showbu:false,
+           text:texttemp,
+           color:colortemp,
+           choose:choosetemp,
+           q:qtemp
+         });
+        
+         var that=this;
+         setTimeout(function () {
+           that.setData({
+             show:false,
+             showq:true
+           });
+         that.start();
+          }, 1500) //延迟时间 这里是1秒
+   }else if(this.data.indexq<15){
+    var texttemp=[];
+    texttemp.push(Math.floor(Math.random()*6));
+    texttemp.push(Math.floor(Math.random()*6));
+    var colortemp=[];
+    colortemp.push(Math.floor(Math.random()*6));
+    colortemp.push(Math.floor(Math.random()*6));
+    var choosetemp=[];
+    var qtemp=this.data.allq[Math.floor(Math.random()*4+2)];
+     if(qtemp=="出现的第一个文字是什么？")
+     {
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+          
+             if(choosetemp[j]==num||num==texttemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+ 
+       
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+       }
+       choosetemp[Math.floor(Math.random()*4)]=texttemp[0];
+       this.setData({
+         ans:texttemp[0],
+        });
+     }else if(qtemp=="出现的第一个文字是什么颜色？"){
+       for(var i=0;i<4;){
+         var num=Math.floor(Math.random()*6);
+         var f=0;
+         for(var j=0;j<choosetemp.length;j++)
+         {
+          
+             if(choosetemp[j]==num||num==colortemp[0])
+             {
+               f=1;
+               break;
+             }
+         }
+         if(f==0)
+         {
+             choosetemp.push(num);
+             i++;
+         }
+ 
+        
+       }
+       choosetemp[Math.floor(Math.random()*4)]=colortemp[0];
+       this.setData({
+        ans:colortemp[0],
+       });
+     }else if(qtemp=="出现的第二个文字是什么颜色？"){
+      for(var i=0;i<4;){
+        var num=Math.floor(Math.random()*6);
+        var f=0;
+        for(var j=0;j<choosetemp.length;j++)
+        {
+         
+            if(choosetemp[j]==num||num==colortemp[1])
+            {
+              f=1;
+              break;
+            }
+        }
+        if(f==0)
+        {
+            choosetemp.push(num);
+            i++;
+        }
+
+       
+      }
+      choosetemp[Math.floor(Math.random()*4)]=colortemp[1];
+      this.setData({
+       ans:colortemp[1],
+      });
+    }else{
+      for(var i=0;i<4;){
+        var num=Math.floor(Math.random()*6);
+        var f=0;
+        for(var j=0;j<choosetemp.length;j++)
+        {
+         
+            if(choosetemp[j]==num||num==texttemp[1])
+            {
+              f=1;
+              break;
+            }
+        }
+        if(f==0)
+        {
+            choosetemp.push(num);
+            i++;
+        }
+
+       
+      }
+      choosetemp[Math.floor(Math.random()*4)]=texttemp[1];
+      this.setData({
+       ans:texttemp[1],
+      });
+    }
+ 
+ 
+    
+         this.setData({
+           show:true,
+           showbu:false,
+           text:texttemp,
+           color:colortemp,
+           choose:choosetemp,
+           q:qtemp
+         });
+        
+         var that=this;
+         setTimeout(function () {
+           that.setData({
+             show:false,
+             showq:true
+           });
+         that.start();
+          }, 2000) //延迟时间 这里是1秒
+   }
+  
 
       
   },
 
   click:function(e){
       var index=e.currentTarget.dataset.index;
-      var item=e.currentTarget.dataset.item.true;
       this.stop();
      
-      if(item==index){
+      if(this.data.ans==index){
         this.setData({
-          ask:"答案正确"
+          ask:"答案正确",   
+          askcolor:"green",
+          right:this.data.right+1
          });
       }else{
         this.setData({
@@ -90,7 +362,27 @@ Page({
  
         this.setData({
          dis:true,
+        
         });
+
+        var that=this
+        setTimeout(function () {
+          that.setData({
+            show:false,
+            showq:false,
+            showbu:true,
+            dis:false,
+            ask:"",
+            indexq:that.data.indexq+1
+          });
+          that.show();
+         }, 2000)
+
+         if(this.data.indexq==16)
+         {
+           this.stop();
+         }
+      
 
 
     
@@ -102,12 +394,7 @@ Page({
     //停止（暂停）
     clearInterval(intt);
     //时间重置
-    that.setData({
-      hour: 0,
-      minute: 0,
-      second: 0,
-      millisecond: 0,
-    })
+   
     intt = setInterval(function () { that.timer() }, 50);
   },
   //暂停
@@ -128,7 +415,7 @@ Page({
   },
   timer: function () {
     var that = this;
-    console.log(that.data.millisecond)
+   
     that.setData({
       millisecond: that.data.millisecond + 5
     })
